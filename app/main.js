65e5d0c4aa42e07b85885502a8340c10cb2fcb6b
Dennis Ranke <dennis.ranke@gmail.com>
2014-10-05 17:55:41 +0200

    implement basic recording/playback proof-of-concept

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 require(['libs/react-0.11.2.js', 'vorbisrecorder'], function(React, VorbisRecorder) {
   var vorbisRecorder = new VorbisRecorder();
+  var audio = new AudioContext();
   var App = React.createClass({
     getInitialState: function() {
       return { ready: false };
@@ -10,12 +11,43 @@ require(['libs/react-0.11.2.js', 'vorbisrecorder'], function(React, VorbisRecord
     setReady: function() {
       this.setState({ ready: true });
     },
+    startRecording: function() {
+      var self = this;
+      navigator.webkitGetUserMedia({audio: true}, function(stream) {
+        self.setState({
+          recorder: vorbisRecorder.startRecording(
+            stream.getAudioTracks()[0]
+          )
+        });
+      }, function(e) {
+        console.log(e);
+      });
+    },
+    stopRecording: function() {
+      this.state.recorder.stop().then(function(data) {
+        console.log(data.byteLength);
+        audio.decodeAudioData(data, function(buffer) {
+          var source = audio.createBufferSource();
+          source.buffer = buffer;
+          source.connect(audio.destination);
+          source.start();
+        }, function() {
+          console.log('failed to decode audio data');
+        });
+      });
+      this.setState({ recorder: undefined });
+    },
     render: function() {
       var DOM = React.DOM;
       if(!this.state.ready) {
         return DOM.div(null, 'Loading vorbis encoder, please wait...');
       }
-      return DOM.div(null, 'Ready!');
+      return DOM.div(null,
+        'Ready!',
+        this.state.recorder ?
+          DOM.button({type: 'button', onClick: this.stopRecording}, 'Stop recording') :
+          DOM.button({type: 'button', onClick: this.startRecording}, 'Start recording')
+      );
     }
   });
 
